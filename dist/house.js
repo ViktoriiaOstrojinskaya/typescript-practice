2a169077454fb8998493ecee1134d98c394f62e1
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-01-27 10:17:38 -0500

    task #5

--- dist/house.js
+++ dist/house.js
@@ -1,41 +1,41 @@
 "use strict";
 class Key {
-  constructor() {
-    this.signature = Math.random();
-  }
-  getSignature() {
-    return this.signature;
-  }
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature() {
+        return this.signature;
+    }
 }
 class Person {
-  constructor(key) {
-    this.key = key;
-  }
-  getKey() {
-    return this.key;
-  }
+    constructor(key) {
+        this.key = key;
+    }
+    getKey() {
+        return this.key;
+    }
 }
 class House {
-  constructor(key) {
-    this.key = key;
-    this.door = false;
-    this.tenants = [];
-  }
-  comeIn(person) {
-    if (!this.door) {
-      throw new Error("Door is close");
+    constructor(key) {
+        this.key = key;
+        this.door = false;
+        this.tenants = [];
     }
-    this.tenants.push(person);
-    console.log("Person inside");
-  }
+    comeIn(person) {
+        if (!this.door) {
+            throw new Error("Door is close");
+        }
+        this.tenants.push(person);
+        console.log("Person inside");
+    }
 }
 class MyHouse extends House {
-  openDoor(key) {
-    if (key.getSignature() !== this.key.getSignature()) {
-      throw new Error("Key to another door");
+    openDoor(key) {
+        if (key.getSignature() !== this.key.getSignature()) {
+            throw new Error("Key to another door");
+        }
+        return (this.door = true);
     }
-    return (this.door = true);
-  }
 }
 const key = new Key();
 const house = new MyHouse(key);
